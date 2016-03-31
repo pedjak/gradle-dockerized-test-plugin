99b1f2b2a0767d918277f2ae19c7cb35484bb4b7
Predrag Knezevic <predrag.knezevic@datastax.com>
2016-03-31 12:07:36 +0200

    0.3.5: fixed compatability with Gradle 2.12

--- src/main/groovy/com/pedjak/gradle/plugins/dockerizedtest/DockerizedTestPlugin.groovy
+++ src/main/groovy/com/pedjak/gradle/plugins/dockerizedtest/DockerizedTestPlugin.groovy
@@ -16,78 +16,106 @@
 
 package com.pedjak.gradle.plugins.dockerizedtest
 
-import org.gradle.StartParameter
 import org.gradle.api.*
 import org.gradle.internal.concurrent.ExecutorFactory
 import org.gradle.messaging.actor.ActorFactory
-import org.gradle.api.internal.*
 import org.gradle.api.internal.file.*
-import org.gradle.internal.id.*
 import org.gradle.api.tasks.testing.Test
 import org.gradle.api.internal.tasks.testing.detection.*
+import org.gradle.messaging.remote.ConnectionAcceptor
+import org.gradle.messaging.remote.MessagingServer
+import org.gradle.messaging.remote.ObjectConnection
+import org.gradle.messaging.remote.internal.ConnectCompletion
 import org.gradle.messaging.remote.internal.IncomingConnector
-import org.gradle.messaging.remote.internal.MessagingServices
 import org.apache.commons.lang3.SystemUtils
 import org.apache.maven.artifact.versioning.ComparableVersion
-import org.gradle.process.internal.child.EncodedStream
-import org.gradle.util.GUtil
+import org.gradle.messaging.remote.internal.hub.MessageHubBackedObjectConnection
+import org.gradle.process.internal.DefaultWorkerProcessFactory
+import org.gradle.process.internal.ExecHandleFactory
 
 import javax.inject.Inject
 
 class DockerizedTestPlugin implements Plugin<Project> {
 
     def actorFactory
-    def startParameter
-    def incommingConnector
-    def executorFactory
     def resolver
     def currentUser
+    def messagingServer
 
     @Inject
-    DockerizedTestPlugin(
-            StartParameter startParameter,
-            ExecutorFactory executorFactory,
-            MessagingServices messagingServices, FileResolver resolver, ActorFactory actorFactory) {
+    DockerizedTestPlugin(MessagingServer messagingServer, FileResolver resolver, ActorFactory actorFactory) {
         this.actorFactory = actorFactory
-        this.incommingConnector = messagingServices.get(IncomingConnector)
-        this.startParameter = startParameter
-        this.executorFactory = executorFactory
         this.resolver = resolver
         this.currentUser = SystemUtils.IS_OS_WINDOWS ? "0" : "id -u".execute().text.trim()
+        this.messagingServer = new MessageServer(messagingServer.connector, messagingServer.executorFactory)
     }
 
-    void configureTest(project, test, attachStdInContent) {
+    void configureTest(project, test) {
         def ext = test.extensions.create("docker", DockerizedTestExtension, [] as Object[])
+        def startParameter = project.gradle.startParameter
         ext.volumes = [ "$startParameter.gradleUserHomeDir": "$startParameter.gradleUserHomeDir",
                         "$project.projectDir":"$project.projectDir"]
         ext.user = currentUser
         test.doFirst {
             def extension = test.extensions.docker
             if (extension?.image) {
-                test.testExecuter = new DefaultTestExecuter(newProcessBuilderFactory(extension, attachStdInContent, project.gradle.services.get(ClassPathRegistry)), actorFactory);
+                test.testExecuter = new DefaultTestExecuter(newProcessBuilderFactory(extension, test.processBuilderFactory), actorFactory);
             }
         }
     }
 
     void apply(Project project) {
 
+        boolean preGradle2_12 = new ComparableVersion(project.gradle.gradleVersion).compareTo(new ComparableVersion('2.12')) < 0
+        if (preGradle2_12) throw new GradleException("dockerized-test plugin requires Gradle 2.12+")
 
-        boolean preGradle2_4 = new ComparableVersion(project.gradle.gradleVersion).compareTo(new ComparableVersion('2.4')) < 0
-        def attachStdInContent = preGradle2_4 ? { workerFactory, javaCommand ->
-            def bytes = new ByteArrayOutputStream();
-            def encoded = new EncodedStream.EncodedOutput(bytes);
-            GUtil.serialize(workerFactory.create(), encoded);
-            def stdinContent = new ByteArrayInputStream(bytes.toByteArray());
-            javaCommand.setStandardInput(stdinContent);
-        } : { workerFactory, javaCommand -> /* no-op */ }
-
-        project.tasks.withType(Test).each { test -> configureTest(project, test, attachStdInContent) }
+        project.tasks.withType(Test).each { test -> configureTest(project, test) }
         project.tasks.whenTaskAdded { task ->
-            if (task instanceof Test) configureTest(project, task, attachStdInContent)
+            if (task instanceof Test) configureTest(project, task)
         }
     }
 
-    def newProcessBuilderFactory(extension, attachStdInContent, classPathRegistry) {
-        new DockerizedWorkerProcessFactory(startParameter.logLevel, incommingConnector, executorFactory, classPathRegistry, resolver, extension, new LongIdGenerator(), attachStdInContent)
+    def newProcessBuilderFactory(extension, defaultProcessBuilderFactory) {
+        def execHandleFactory = [newJavaExec: { -> new DockerizedJavaExecHandleBuilder(extension, resolver)}] as ExecHandleFactory
+        new DefaultWorkerProcessFactory(defaultProcessBuilderFactory.workerLogLevel,
+                                        //defaultProcessBuilderFactory.server,
+                                        messagingServer,
+                                        defaultProcessBuilderFactory.systemClassLoaderWorkerFactory.classPathRegistry,
+                                        defaultProcessBuilderFactory.idGenerator,
+                                        defaultProcessBuilderFactory.gradleUserHomeDir,
+                                        defaultProcessBuilderFactory.systemClassLoaderWorkerFactory.temporaryFileProvider,
+                                        execHandleFactory
+                                        )
     }
+
+    class MessageServer implements MessagingServer {
+        def IncomingConnector connector;
+        def ExecutorFactory executorFactory;
+
+        public MessageServer(IncomingConnector connector, ExecutorFactory executorFactory) {
+            this.connector = connector;
+            this.executorFactory = executorFactory;
+        }
+
+        public ConnectionAcceptor accept(Action<ObjectConnection> action) {
+            return this.connector.accept(new ConnectEventAction(action, executorFactory), true);
+        }
+
+
+    }
+
+    class ConnectEventAction implements Action<ConnectCompletion> {
+        def action;
+        def executorFactory;
+
+        public ConnectEventAction(Action<ObjectConnection> action, executorFactory) {
+            this.executorFactory = executorFactory
+            this.action = action
+        }
+
+        public void execute(ConnectCompletion completion) {
+            action.execute(new MessageHubBackedObjectConnection(executorFactory, completion));
+        }
+    }
+
 }
